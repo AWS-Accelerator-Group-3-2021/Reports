4f752331a7131abc5dbec50437dcc127f664a54d
Prakhar Trivedi <prakh0706@outlook.com>
2021-12-15 10:26:39 +0530

    fixed memory location issue thanks to @CedricKhuaYanHan
    the issue prevented the no reports found para from being shown

--- list.js
+++ list.js
@@ -1,17 +1,21 @@
-const currentAuthToken = location.pathname.substring(6)
+var currentAuthToken = location.pathname.split('/')
+currentAuthToken = currentAuthToken[2]
 var url = `http://localhost:8000/session/${currentAuthToken}/list/meta/reports`
+console.log(url)
 axios.get(url)
     .then(function (response) {
         // handle success, reports given in json format
-        if (response.data == {}) {
+        console.log(typeof {})
+        console.log(response.data)
+        console.log(Object.keys(response.data).length === 0)
+        if (Object.keys(response.data).length === 0) {
+            console.log('i have run')
             const para = document.createElement("p");
             para.innerHTML = "No reports found.";
             const element = document.getElementById("reportsList");
             element.appendChild(para);
         } else {
-            response.forEach((report) => {
-                console.log(report)
-            })
+            
         }
     })
     .catch(function (error) {
